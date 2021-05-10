d2e0d7c27b6e1fddf88a31a849dcbe25d7e649fd
WonDongGyun <waterflame1@naver.com>
2021-05-11 01:07:24 +0900

    닷env

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -2,6 +2,7 @@ import { MailerModule, MailerService } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
+require('dotenv').config();
 
 @Module({
 	imports: [
