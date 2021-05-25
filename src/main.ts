0bf8258a849699a3c5f8100e12b5336b4781fe90
WonDongGyun <52685665+WonDongGyun@users.noreply.github.com>
2021-05-25 15:56:59 +0900

    https 설정

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
+	const httpsOptions = {
+		key: fs.readFileSync('./secrets/private-key.pem'),
+		cert: fs.readFileSync('./secrets/public-certificate.pem')
+	};
 	const app = await NestFactory.create(AppModule, {
-		cors: true
+		cors: true,
+		httpsOptions
 	});
 	app.useGlobalPipes(
 		new ValidationPipe({
