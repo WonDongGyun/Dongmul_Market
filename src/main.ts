f829cd253a8270d12c3f83d9ca8091fb889b5c37
WonDongGyun <52685665+WonDongGyun@users.noreply.github.com>
2021-05-26 00:15:07 +0900

    commit

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,10 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
-import {
-	ExpressAdapter,
-	NestExpressApplication
-} from '@nestjs/platform-express';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import express = require('express');
-import http = require('http');
-import https = require('https');
 
 async function bootstrap() {
-	const server = express();
-
 	const httpsOptions = {
 		key: fs.readFileSync(
 			'/etc/letsencrypt/live/dongmul.shop/privkey.pem',
@@ -28,14 +20,10 @@ async function bootstrap() {
 			'utf8'
 		)
 	};
-	const app = await NestFactory.create<NestExpressApplication>(
-		AppModule,
-		new ExpressAdapter(server),
-		{
-			cors: true,
-			httpsOptions
-		}
-	);
+	const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+		cors: true,
+		httpsOptions
+	});
 
 	app.useStaticAssets(join(__dirname, '..', 'public'));
 	app.useGlobalPipes(
@@ -45,10 +33,6 @@ async function bootstrap() {
 			transform: true
 		})
 	);
-
-	await app.init();
-
-	http.createServer(server).listen(3001);
-	https.createServer(httpsOptions, server).listen(3000);
+	await app.listen(3000);
 }
 bootstrap();
